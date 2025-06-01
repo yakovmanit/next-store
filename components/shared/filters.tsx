9c6a8342975e96d83f9ccac81d1e134f483861fc
yakiv <yakov.manitskiy@gmail.com>
2025-06-01 17:04:53 +0300

    filters: add range slider states

--- components/shared/filters.tsx
+++ components/shared/filters.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React from 'react';
+import React, {useState} from 'react';
 import {Title, FilterCheckbox, RangeSlider, CheckboxFiltersGroup} from "./index";
 import { Input } from '../ui';
 import {useFilterIngredients} from "@/hooks/useFilterIngredients";
@@ -9,8 +9,24 @@ interface Props {
   className?: string;
 }
 
+interface PriceProps {
+  priceFrom: number;
+  priceTo: number;
+}
+
 export const Filters: React.FC<Props> = ({ className }) => {
   const { ingredients, loading, onAddId, selectedIds } = useFilterIngredients();
+  const [prices, setPrice] = useState<PriceProps>({
+    priceFrom: 0,
+    priceTo: 2000
+  });
+
+  const updatePrice = (name: keyof PriceProps, value: number) => {
+    setPrice({
+      ...prices,
+      [name]: value,
+    })
+  };
 
   const items = ingredients.map(item => ({ text: item.name, value: String(item.id) }));
 
@@ -32,14 +48,17 @@ export const Filters: React.FC<Props> = ({ className }) => {
             type="number"
             placeholder="0"
             min={0}
-            max={1000}
-            defaultValue={0}
+            max={2000}
+            value={String(prices.priceFrom)}
+            onChange={e => updatePrice('priceFrom', Number(e.target.value))}
           />
           <Input
             type="number"
             min={100}
-            max={1000}
+            max={2000}
             placeholder="1000"
+            value={String(prices.priceTo)}
+            onChange={e => updatePrice('priceTo', Number(e.target.value))}
           />
         </div>
 
@@ -47,7 +66,11 @@ export const Filters: React.FC<Props> = ({ className }) => {
           min={0}
           max={1000}
           step={10}
-          value={[0, 1000]}
+          value={[
+            prices.priceFrom,
+            prices.priceTo,
+          ]}
+          onValueChange={([priceFrom, priceTo]) => setPrice({ priceFrom, priceTo })}
         />
       </div>
 
