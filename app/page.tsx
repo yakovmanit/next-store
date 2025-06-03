3c7d78d34ddf24b5289a062301a62683ebe018a6
yakiv <yakov.manitskiy@gmail.com>
2025-06-03 23:35:59 +0300

    home page: add dynamic products and categories

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,26 @@
 import {Container, TopBar, Title, Filters} from "@/components/shared";
 import {ProductsGroupList} from "@/components/shared/products-group-list";
+import {prisma} from "@/prisma/prisma-client";
 
-export default function Home() {
+export default async function Home() {
+  const categories = await prisma.category.findMany({
+    include: {
+      products: {
+        include: {
+          ingredients: true,
+          items: true,
+        }
+      },
+    },
+  });
+
   return (
     <>
       <Container className="mt-8">
         <Title size={"lg"} text={'All products'} className="font-semibold" />
       </Container>
 
-      <TopBar />
+      <TopBar categories={categories.filter(category => category.products.length > 0)} />
 
       <Container className="mt-10 pb-14">
         <div className="flex gap-15">
@@ -20,55 +32,19 @@ export default function Home() {
 
           {/* Products list */}
           <div className="flex-1">
-            <div className="flex flex-const gap-16 mb-16">
-              <ProductsGroupList categoryId={1} title="Coffee blend" items={[
-                {
-                  id: 1,
-                  name: 'Colombia coffee',
-                  imageUrl: 'https://www.elitecoffee.od.ua/wp-content/uploads/2023/05/SUMMER-1-KG.png',
-                  price: 550,
-                  items: [{ price: 500 }],
-                },
-                {
-                  id: 2,
-                  name: 'Colombia coffee',
-                  imageUrl: 'https://www.elitecoffee.od.ua/wp-content/uploads/2023/05/SUMMER-1-KG.png',
-                  price: 550,
-                  items: [{ price: 550 }],
-                },
-                {
-                  id: 3,
-                  name: 'Colombia coffee',
-                  imageUrl: 'https://www.elitecoffee.od.ua/wp-content/uploads/2023/05/SUMMER-1-KG.png',
-                  price: 550,
-                  items: [{ price: 600 }],
-                },
-                {
-                  id: 4,
-                  name: 'Colombia coffee',
-                  imageUrl: 'https://www.elitecoffee.od.ua/wp-content/uploads/2023/05/SUMMER-1-KG.png',
-                  price: 550,
-                  items: [{ price: 550 }],
-                },
-              ]} />
-            </div>
-            <div className="flex flex-const gap-16 mb-10">
-              <ProductsGroupList categoryId={2} title="Coffee monosort" items={[
-                {
-                  id: 5,
-                  name: 'Colombia coffee',
-                  imageUrl: 'https://www.elitecoffee.od.ua/wp-content/uploads/2023/05/SUMMER-1-KG.png',
-                  price: 550,
-                  items: [{ price: 550 }],
-                },
-                {
-                  id: 6,
-                  name: 'Colombia coffee',
-                  imageUrl: 'https://www.elitecoffee.od.ua/wp-content/uploads/2023/05/SUMMER-1-KG.png',
-                  price: 550,
-                  items: [{ price: 550 }],
-                },
-              ]} />
+            <div className="flex flex-col gap-16 mb-20">
+              {
+                categories.map((category) => (
+                  category.products.length > 0 && (
+                    <ProductsGroupList
+                      key={category.id}
+                      categoryId={category.id}
+                      title={category.name}
+                      items={category.products}
+                    />
+                  )
+                ))
+              }
             </div>
           </div>
 
--- components/shared/categories.tsx
+++ components/shared/categories.tsx
@@ -3,25 +3,20 @@
 import React from 'react';
 import {cn} from "@/lib/utils";
 import {useCategoryStore} from "@/store/category";
+import {Category} from "@prisma/client";
 
 interface Props {
   className?: string;
+  items: Category[];
 }
 
-const cats = [
-  { id: 1, name: 'Coffee blend' },
-  { id: 2, name: 'Coffee monosort' },
-  { id: 3, name: 'Drips' },
-  { id: 4, name: 'Sets' },
-];
-
-export const Categories: React.FC<Props> = ({ className }) => {
+export const Categories: React.FC<Props> = ({ items, className }) => {
   const categoryActiveId = useCategoryStore(state => state.activeId);
 
   return (
     <div className={cn('inline-flex gap-1 bg-gray-50 p-1 rounded-2xl', className)}>
       {
-        cats.map(({ id, name }, index) => (
+        items.map(({ id, name }, index) => (
           <a className={cn(
             'flex items-center font-bold h-11 rounded-2xl px-5 cursor-pointer',
             categoryActiveId === id && 'bg-white shadow-2xl text-primary'
