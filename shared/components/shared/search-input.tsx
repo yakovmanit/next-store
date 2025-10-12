61535823f12367cae8dcb6189cb008713e511b93
yakiv <yakov.manitskiy@gmail.com>
2025-10-12 23:41:29 +0300

    fix(search): fix product link

--- shared/components/shared/search-input.tsx
+++ shared/components/shared/search-input.tsx
@@ -67,7 +67,7 @@ export const SearchInput: React.FC<Props> = ({ className }) => {
                 products.map(product => (
                   <Link
                     key={product.id}
-                    href={`/app/(root)/product/${product.id}`}
+                    href={`/product/${product.id}`}
                     onClick={onClickItem}
                     className="flex items-center gap-3 w-full px-3 py-2 hover:bg-primary/10"
                   >
