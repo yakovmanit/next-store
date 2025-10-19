3f2ca60319e5f8d3d5cf2cae68adc6fb7316dffe
yakiv <yakov.manitskiy@gmail.com>
2025-10-19 12:56:05 +0300

    refactor(checkout): add new separate checkout blocks to page

--- app/(checkout)/checkout/page.tsx
+++ app/(checkout)/checkout/page.tsx
@@ -2,18 +2,14 @@
 
 import React from 'react';
 import {
-  CheckoutItem,
   CheckoutSidebar,
   Container,
   Title,
-  WhiteBlock
+  CheckoutCart,
+  CheckoutPersonalForm,
+  CheckoutAddressForm
 } from "@/shared/components/shared";
-import {Input, Textarea} from "@/shared/components/ui";
 import {useCart} from "@/shared/hooks";
-import {getCartItemDetails} from "@/shared/lib";
-import {CoffeeSize, CoffeeType} from "@/shared/constants/coffee";
-
-
 
 export default function CheckoutPage() {
   const {
@@ -36,52 +32,16 @@ export default function CheckoutPage() {
       <div className="flex gap-10">
         {/* Left part */}
         <div className="flex flex-col gap-10 flex-1 mb-20">
-          <WhiteBlock title="1. Cart">
-            <div className="flex flex-col gap-4">
-              {
-                items.map((item) => (
-                  <CheckoutItem
-                    key={item.id}
-                    id={item.id}
-                    name={item.name}
-                    price={item.price}
-                    quantity={item.quantity}
-                    imageUrl={item.imageUrl}
-                    details={
-                      getCartItemDetails(
-                        item.ingredients,
-                        item.coffeeType as CoffeeType,
-                        item.coffeeSize as CoffeeSize,
-                      )
-                    }
-                    disabled={item.disabled}
-                    onClickCountButton={(type) => onClickCountButton(item.id, item.quantity, type)}
-                    onClickRemove={() => removeCartItem(item.id)}
-                  />
-                ))
-              }
-            </div>
-          </WhiteBlock>
 
-          <WhiteBlock title="2. Your personal data">
-            <div className="grid grid-cols-2 gap-5">
-              <Input name="firstName" className="text-base" placeholder="Name" />
-              <Input name="lastName" className="text-base" placeholder="Surneme" />
-              <Input name="email" className="text-base" placeholder="E-Mail" />
-              <Input name="phone" className="text-base" placeholder="Phone number" />
-            </div>
-          </WhiteBlock>
+          <CheckoutCart
+            items={items}
+            onClickCountButton={onClickCountButton}
+            removeCartItem={removeCartItem}
+          />
 
-          <WhiteBlock title="3. Your address">
-            <div className="flex flex-col gap-5">
-              <Input name="firstName" className="text-base" placeholder="Enter your address" />
-              <Textarea
-                className="text-base"
-                placeholder="Your comment for order"
-                rows={5}
-              />
-            </div>
-          </WhiteBlock>
+          <CheckoutPersonalForm />
+
+          <CheckoutAddressForm />
         </div>
 
         {/* Right part */}
