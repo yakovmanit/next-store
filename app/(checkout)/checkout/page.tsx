bd62c3b8d02c4e392ccdab0fd95474dcb29d00de
yakiv <yakov.manitskiy@gmail.com>
2025-10-20 13:55:13 +0300

    feat(checkout): add RHF to FormInput component, add FormProvider to display errors when form submited

--- app/(checkout)/checkout/page.tsx
+++ app/(checkout)/checkout/page.tsx
@@ -7,9 +7,18 @@ import {
   Title,
   CheckoutCart,
   CheckoutPersonalForm,
-  CheckoutAddressForm
+  CheckoutAddressForm,
 } from "@/shared/components/shared";
 import {useCart} from "@/shared/hooks";
+import {
+  FormProvider,
+  useForm,
+} from "react-hook-form";
+import {
+  checkoutFormSchema,
+  CheckoutFormValues,
+} from "@/shared/constants";
+import {zodResolver} from "@hookform/resolvers/zod";
 
 export default function CheckoutPage() {
   const {
@@ -18,6 +27,22 @@ export default function CheckoutPage() {
     items,
     removeCartItem
   } = useCart();
+
+  const form = useForm<CheckoutFormValues>({
+    resolver: zodResolver(checkoutFormSchema),
+    defaultValues: {
+      email: '',
+      firstName: '',
+      lastName: '',
+      phone: '',
+      address: '',
+      comment: '',
+    },
+  });
+
+  const onSubmit = (data: CheckoutFormValues) => {
+    console.log(data);
+  }
 
   const onClickCountButton = (id: number, quantity: number, type: 'plus' | 'minus') => {
     const newQuantity = type === 'plus' ? quantity + 1 : quantity - 1;
@@ -29,24 +54,28 @@ export default function CheckoutPage() {
     <Container className="mt-10">
       <Title text="Checkout" className="font-extrabold mb-8 text-[36px]" />
 
-      <div className="flex gap-10">
-        {/* Left part */}
-        <div className="flex flex-col gap-10 flex-1 mb-20">
+      <FormProvider {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <div className="flex gap-10">
+            {/* Left part */}
+            <div className="flex flex-col gap-10 flex-1 mb-20">
 
-          <CheckoutCart
-            items={items}
-            onClickCountButton={onClickCountButton}
-            removeCartItem={removeCartItem}
-          />
+              <CheckoutCart
+                items={items}
+                onClickCountButton={onClickCountButton}
+                removeCartItem={removeCartItem}
+              />
 
-          <CheckoutPersonalForm />
+              <CheckoutPersonalForm />
 
-          <CheckoutAddressForm />
-        </div>
+              <CheckoutAddressForm />
+            </div>
 
-        {/* Right part */}
-        <CheckoutSidebar totalAmount={totalAmount}/>
-      </div>
+            {/* Right part */}
+            <CheckoutSidebar totalAmount={totalAmount}/>
+          </div>
+        </form>
+      </FormProvider>
     </Container>
   );
 };
--- shared/components/shared/form/form-input.tsx
+++ shared/components/shared/form/form-input.tsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import {ClearButton, ErrorText, RequiredSymbol} from "@/shared/components/shared";
 import {Input} from "@/shared/components/ui";
+import {useFormContext} from "react-hook-form";
 
 interface Props extends React.InputHTMLAttributes<HTMLInputElement>{
   name: string;
@@ -12,6 +13,20 @@ interface Props extends React.InputHTMLAttributes<HTMLInputElement>{
 }
 
 export const FormInput: React.FC<Props> = ({ className, name, label, required, ...props }) => {
+  const {
+    register,
+    formState: { errors },
+    watch,
+    setValue,
+  } = useFormContext();
+
+  const value = watch(name);
+  const errorText = errors[name]?.message as string;
+
+  const onClickClear = () => {
+    setValue(name, '', { shouldValidate: true });
+  }
+
   return (
     <div className={className}>
       {label && (
@@ -21,12 +36,16 @@ export const FormInput: React.FC<Props> = ({ className, name, label, required, .
       )}
 
       <div className="relative">
-        <Input className="h-12 text-md" {...props} />
+        <Input className="h-12 text-md" {...register(name)} {...props} />
 
-        <ClearButton />
+        {value && (
+          <ClearButton onClick={onClickClear} />
+        )}
       </div>
 
-      <ErrorText text="Field required" className="mt-2" />
+      {errorText && (
+        <ErrorText text={errorText} className="mt-2" />
+      )}
     </div>
   );
 };
