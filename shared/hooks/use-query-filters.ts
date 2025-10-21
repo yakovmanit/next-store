309eb8713c67a729964ba64cdcd64756f3febd2a
yakiv <yakov.manitskiy@gmail.com>
2025-10-22 00:36:09 +0300

    fix(filters): don't execute useQueryFilters useEffect when first mounting

--- shared/hooks/use-query-filters.ts
+++ shared/hooks/use-query-filters.ts
@@ -1,24 +1,29 @@
 import {useRouter} from "next/navigation";
-import {useEffect} from "react";
+import {useEffect, useRef} from "react";
 import qs from "qs"
 import {Filters} from "./use-filters";
 
 export const useQueryFilters = (filters: Filters) => {
+  const isMounted = useRef(false);
   const router = useRouter();
 
   useEffect(() => {
-    const params = {
-      ...filters.prices,
-      productTypes: Array.from(filters.productTypes),
-      sizes: Array.from(filters.sizes),
-      ingredients: Array.from(filters.selectedIngredients),
+    if (isMounted.current) {
+      const params = {
+        ...filters.prices,
+        productTypes: Array.from(filters.productTypes),
+        sizes: Array.from(filters.sizes),
+        ingredients: Array.from(filters.selectedIngredients),
+      }
+
+      const query = qs.stringify(params, {
+        arrayFormat: 'comma',
+      });
+
+      router.push(`?${query}`, { scroll: false });
     }
 
-    const query = qs.stringify(params, {
-      arrayFormat: 'comma',
-    });
-
-    router.push(`?${query}`, { scroll: false });
+    isMounted.current = true;
 
   }, [filters.sizes, filters.prices, filters.selectedIngredients, filters.productTypes, router]);
 }
