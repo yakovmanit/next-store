dc1fde51df4096d2180e2e726c905a59166acf1d
yakiv <yakov.manitskiy@gmail.com>
2025-10-24 21:55:56 +0300

    fix(prisma): configure PrismaClient with custom datasource and logging
    - Added support for database URL through environment variable.
    - Enabled conditional logging for development and production environments.

--- prisma/prisma-client.ts
+++ prisma/prisma-client.ts
@@ -1,7 +1,14 @@
 import { PrismaClient } from '@prisma/client';
 
 const prismaClientSingleton = () => {
-  return new PrismaClient();
+  return new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
+    log: process.env.NODE_ENV === 'development' ? ['warn', 'error'] : ['error'],
+  });
 };
 
 declare global {
