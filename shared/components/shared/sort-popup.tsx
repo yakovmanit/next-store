bfb3ce454c4601f9e522fe2cce16dbb15ea30217
yakiv <yakov.manitskiy@gmail.com>
2025-10-24 21:23:08 +0300

    chore(ui): comment out unused sort popup elements
    - Temporarily removed `ArrowUpDown` and related elements in `SortPopup` component.
    - Added TODO for implementing sort logic in the future.

--- shared/components/shared/sort-popup.tsx
+++ shared/components/shared/sort-popup.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {cn} from "@/shared/lib/utils";
-import {ArrowUpDown} from "lucide-react";
+// import {ArrowUpDown} from "lucide-react";
 
 interface Props {
   className?: string;
@@ -9,9 +9,10 @@ interface Props {
 export const SortPopup: React.FC<Props> = ({ className }) => {
   return (
     <div className={cn('inline-flex items-center gap-1 bg-gray-50 px-5 h-[54px] rounded-2xl cursor-pointer', className)}>
-      <ArrowUpDown size={16} />
-      <b>Sort by:</b>
-      <b className="text-primary">popularity</b>
+      {/* TODO: add sort logic */}
+      {/*<ArrowUpDown size={16} />*/}
+      {/*<b>Sort by:</b>*/}
+      {/*<b className="text-primary">popularity</b>*/}
     </div>
   );
 };
